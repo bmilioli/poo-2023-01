fb6d2f7417e580fc3065c4b1cb77e6bc4ed7fa08
Bmilioli <101820274+bmilioli@users.noreply.github.com>
2023-05-29 21:52:18 -0300

    t08

---
+++ t08/t08/src/main/java/com/github/bmilioli/poo/t08/Projeto.java
@@ -0,0 +1,7 @@
+package com.github.bmilioli.poo.t08;
+
+public class Projeto {
+    public static void main(String[] args) {
+        LinguagemProgramacao linguagens = new LinguagemProgramacao();
+    }
+}
