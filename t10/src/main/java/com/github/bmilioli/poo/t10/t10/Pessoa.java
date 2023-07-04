efca8677230adba36629fdf46001e8387e12e058
Bmilioli <101820274+bmilioli@users.noreply.github.com>
2023-07-03 21:29:52 -0300

    t10

--- t10/src/main/java/com/github/bmilioli/poo/t10/t10/Pessoa.java
+++ t10/src/main/java/com/github/bmilioli/poo/t10/t10/Pessoa.java
@@ -4,14 +4,29 @@ public class Pessoa {
     String nome;
     int idade;
     String cpf;
-    String rg;
-    String endereco;
-    String telefone;
-    String email;
-    String sexo;
-    String estadoCivil;
-    String profissao;
-    String escolaridade;
-    String nacionalidade;
 
+    String sexo;
+
+    public Pessoa(String nome, int idade, String cpf, String sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
 }
